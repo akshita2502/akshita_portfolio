15f4c2fa5647ac0592a527a06a213304045fbb2c
akshita2502 <akshitagunturi@gmail.com>
2025-07-26 23:47:54 +0530

    navbar color and blurr effects

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -2,9 +2,8 @@ import { Link } from "react-scroll";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-black/70 backdrop-blur-sm text-white z-20 shadow-md">
+    <nav className="fixed top-0 left-0 w-full bg-gray-800/70 backdrop-blur-sm text-white z-20 shadow-md">
       <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Harshini Gunturi</h1>
         <ul className="flex space-x-6">
           {["home", "about", "projects", "contact"].map((section) => (
             <li key={section}>
