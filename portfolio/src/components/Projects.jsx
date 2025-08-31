011c1ea8a934c3364aaa4fcf81ce29a55f407b9c
akshita2502 <akshitagunturi@gmail.com>
2025-08-31 23:37:01 +0530

    code changes for deployment: refactor project card, add profile photo

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -5,40 +5,49 @@ import myportfolioImage from "../assets/images/portfoliosc.png";
 import ParticleBackground from "./ParticleBackground";
 
 const projects = [
+  // {
+  //   image: empImage,
+  //   title: "CRUD operations - Employee Management System",
+  //   description:
+  //     "Allows admins to add, update, delete, and view employee details like name, department, salary, and designation.",
+  //   tech: ["React", "FastAPI", "Python", "SQLAlchemy", "HTML", "CSS"],
+  //   demoLink: "https://readme-gen-demo-link.com",
+  //   githubLink: "https://github.com/harshini0804/CRUD-Project.git",
+  // },
   {
-    image: empImage,
-    title: "CRUD operations - Employee Management System",
-    description:
-      "Allows admins to add, update, delete, and view employee details like name, department, salary, and designation.",
-    tech: ["React", "FastAPI", "Python", "SQLAlchemy", "HTML", "CSS"],
-    demoLink: "https://readme-gen-demo-link.com",
-    githubLink: "https://github.com/harshini0804/CRUD-Project.git",
-  },
-  {
-    image: mylandingImage,
-    title: "Task Pilot - Personal Task Manager",
+    // image: mylandingImage,
+    title: "TaskPilot - Personal Task Manager",
     description:
       "A full-featured task management system supporting user registration/login, task creation, update, deletion, filtering, and status management. Tasks can be marked as completed or archived, with dynamic tables and modal-based task creation.",
-    tech: ["FastAPI", "HTML", "CSS", "JavaScript", "SQLAlchemy"],
-    demoLink: "https://ats-checker-demo.com",
-    githubLink: "https://github.com/harshini0804/Project-1.git",
+    tech: ["FastAPI","ReactJS", "HTML", "CSS", "JavaScript", "PostgreSQL", "SQLAlchemy"],
+    // demoLink: "https://ats-checker-demo.com",
+    // githubLink: "https://github.com/harshini0804/Project-1.git",
   },
 
   {
-    image: myportfolioImage,
+    // image: myportfolioImage,
     title: "My Portfolio",
     description:
       "A curated collection of my full stack development projects built with modern technologies like React, FastAPI, and Tailwind CSS.Each project showcases my skills in designing, developing, and deploying scalable applications from scratch.",
-    tech: ["FastAPI", "HTML", "CSS", "JavaScript","TailWindCSS"],
+    tech: ["FastAPI", "ReactJS", "JavaScript","TailWindCSS"],
     demoLink: "https://ats-checker-demo.com",
     githubLink: "https://github.com/harshini0804/Project-1.git",
   },
   {
     image: myportfolioImage,
-    title: "My Portfolio",
+    title: "ContentCrafter - AI-Powered Content Generator",
     description:
-      "A curated collection of my full stack development projects built with modern technologies like React, FastAPI, and Tailwind CSS.Each project showcases my skills in designing, developing, and deploying scalable applications from scratch.",
-    tech: ["FastAPI", "HTML", "CSS", "JavaScript","TailWindCSS"],
+      " AI-powered app that generates real-time course content using transformer models with a React preview/save interface, automating manual content creation, reducing cotent preparation time, and improving content organization.",
+    tech: ["FastAPI","ReactJS", "CSS", "JavaScript","TailWindCSS", "PostgreSQL", "HuggingFace"],
+    demoLink: "https://ats-checker-demo.com",
+    githubLink: "https://github.com/harshini0804/Project-1.git",
+  },
+  {
+    image: myportfolioImage,
+    title: "SpendWise - Expense Tracker",
+    description:
+      "Finance tracker with recurring transactions, categorization, and interactive dashboards for real-time insights, enhancing personal finance visibility, reducing manual tracking and improving budgeting accuracy.",
+    tech: ["FastAPI", "ReactJS", "CSS", "JavaScript","TailWindCSS", "PostgreSQL"],
     demoLink: "https://ats-checker-demo.com",
     githubLink: "https://github.com/harshini0804/Project-1.git",
   },
